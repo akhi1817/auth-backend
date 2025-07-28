0aae25a2d20c146036131f2513e3c7c597d7ae6d
akhi1817 <akhileshkumbhar2207@gmail.com>
2025-07-28 18:36:01 +0530

    cookie problem solved

--- controllers/userController.js
+++ controllers/userController.js
@@ -75,7 +75,7 @@ const loginUser=async(req,res)=>{
         res.cookie('token',token ,{
             httpOnly:true,
             secure:true,
-            sameSite:'Lax',
+            sameSite:'None',
             maxAge:24*60*60*1000,
         })
 
