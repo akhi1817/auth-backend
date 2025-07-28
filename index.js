2f0410d06bee9c9e0f91d78191f4b7ee9ce6ab11
akhi1817 <akhileshkumbhar2207@gmail.com>
2025-07-28 18:05:13 +0530

    initial commit

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ app.use('/api/auth',authRoutes);
 app.use('/api/auth',otpRoutes);
 
 
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,()=>{
-    console.log(`server is running on ${PORT}`);
-})
+// const PORT=process.env.PORT || 5000;
+// app.listen(PORT,()=>{
+//     console.log(`server is running on ${PORT}`);
+// })
+module.exports=app;
